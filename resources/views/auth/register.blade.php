67b731feb077de83e4a532b66bd6dd629859b0d2
Loir Ricardo Sandoval Martinez <loir.sandoval.29@hotmail.com>
2020-06-30 18:58:42 -0600

    Register fixes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,122 +34,93 @@
   </style>
 </head>
 
-<body>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Registro') }}</div>
+<body class="hold-transition login-page">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('registroUsuarios') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Número de control') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="num_control" type="number" class="form-control" name="num_control" required  autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre(s)') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nombre" type="text" class="form-control" name="nombre" required>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="apellido_paterno" class="col-md-4 col-form-label text-md-right">{{ __('Apellido Paterno') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="apellido_paterno" type="text" class="form-control" name="apellido_paterno" required>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="apellido_materno" class="col-md-4 col-form-label text-md-right">{{ __('Apellido Materno') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="apellido_materno" type="text" class="form-control" name="apellido_materno" required>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" required >
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="plan_estudio" class="col-md-4 col-form-label text-md-right">{{ __('Plan de estudio') }}</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" required name="study_plan_id">
-                                    <option value="1">Comunicación 2000</option>
-                                    <option value="2">Comunicación 2010</option>
-                                    <option value="3">Derecho 1993</option>
-                                    <option value="4">Derecho 2012</option>
-                                    <option value="5">Criminología 2018</option>
-                                    <option value="6">CP y AP 1978</option>
-                                    <option value="7">CP y AP 1995</option>
-                                </select>                            
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Registrarme') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <div class="register-box">
+        <div class="login-logo">
+            <a href="{{ route('login') }}"><h1>Extemporáneos</h1></a>
         </div>
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Registrarme</p>
+                <form method="POST" action="{{ route('registroUsuarios') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input id="num_control" type="number" class="form-control" name="num_control" placeholder="Número de Control" required  autofocus>          
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-tasks"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="nombre" type="text" class="form-control" name="nombre" placeholder="Nombre(s)" required  autofocus>          
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="apellido_paterno" type="text" class="form-control" name="apellido_paterno" placeholder="Apellido Paterno" required  autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="apellido_materno" type="text" class="form-control" name="apellido_materno" placeholder="Apellido Materno" required  autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="email" type="email" class="form-control" name="email" placeholder="Correo" required  autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" required  autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+
+                        <select class="form-control" required name="study_plan_id">
+                                <option value="1">Comunicación 2000</option>
+                                <option value="2">Comunicación 2010</option>
+                                <option value="3">Derecho 1993</option>
+                                <option value="4">Derecho 2012</option>
+                                <option value="5">Criminología 2018</option>
+                                <option value="6">CP y AP 1978</option>
+                                <option value="7">CP y AP 1995</option>
+                        </select>     
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user-graduate"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-success btn btn-block">
+                        {{ __('Registrarme') }}
+                    </button>
+                </form>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
     </div>
-</div>
+    <!-- /.register-box -->
 </body>
+
+       
+
+
